18804c44cc49d8f9d34ba89b425f959ee792f3ed
Harry Kim <harry@bc-labs.net>
2024-10-13 19:19:31 +0900

    Compare json strings

--- testcases/202_serde_json/src/banana/mod.rs
+++ testcases/202_serde_json/src/banana/mod.rs
@@ -22,6 +22,7 @@ struct ErrorResponse {
 #[cfg(test)]
 mod tests {
     use super::{banana_response, ErrorResponse};
+    use serde_json::Value;
 
     #[test]
     fn run_test() {
@@ -29,15 +30,38 @@ mod tests {
     }
 
     #[test]
-    fn another_test() {
+    fn struct_to_json_string() {
         let error_response = ErrorResponse {
             errcode: "M_LIMIT_RATED".to_string(),
-            error: "Rate limited. Try later again".to_string(),
+            error: "Rate limited. Try again later".to_string(),
             retry_after_ms: 2000,
         };
 
         let json = serde_json::to_string(&error_response);
+        let compact_json = r#"{"errcode":"M_LIMIT_RATED","error":"Rate limited. Try again later","retry_after_ms":2000}"#;
 
-        assert!(json.is_ok())
+        assert!(json.is_ok());
+        assert_eq!(json.unwrap(), compact_json);
+    }
+
+    #[test]
+    fn parse_json_string() {
+        let json = r#"{"errcode":"M_LIMIT_RATED","error":"Rate limited. Try again later","retry_after_ms":2000}"#;
+        let v: Value = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(v["errcode"], "M_LIMIT_RATED");
+        assert_eq!(v["error"], "Rate limited. Try again later");
+        assert_eq!(v["retry_after_ms"], 2000);
+
+        // Two ways of decode using from_value()
+        let error_response1: ErrorResponse = serde_json::from_value(v.clone()).unwrap();
+        let error_response2 = serde_json::from_value::<ErrorResponse>(v.clone()).unwrap();
+
+        assert_eq!(error_response1.errcode, error_response2.errcode);
+        assert_eq!(error_response1.error, error_response2.error);
+        assert_eq!(
+            error_response1.retry_after_ms,
+            error_response2.retry_after_ms
+        );
     }
 }
