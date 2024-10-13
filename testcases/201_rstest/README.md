c6ec61c312f89b21ed86b148f27434a2d3bd97de
Harry Kim <harry@bc-labs.net>
2024-10-13 12:32:49 +0900

    Add new test cases

---
+++ testcases/201_rstest/README.md
@@ -0,0 +1,29 @@
+This test case describes how to use `rstest` crate to test.    
+`rstest` is a fixture-based test framework for Rust.  
+
+You can read [rstest documentation](https://docs.rs/rstest/latest/rstest/attr.rstest.html).  
+It says the minimum Rust version for `rstest` is 1.67.1
+
+First you should include `rstest` to `dev-dependencies` of `Cargo.toml`.
+
+``` toml
+[dev-dependencies]
+rstest = "0.23.0"
+```
+
+You can use inside test module:
+
+```rust
+// Module defined here
+// ...
+
+// mod test: Unit tests for the module is defined here
+#[cfg(test)]
+mod test {
+    // dev-dependencies here
+    use rstest::rstest;
+
+    // Test code here
+    // ...
+}
+```
---
+++ testcases/201_rstest/src/main.rs
@@ -0,0 +1,51 @@
+extern crate rstest;
+
+fn main() {
+    // do nothing
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    // Tests not using any crates.
+    #[test]
+    fn test_plus_operation_without_any_dependencies1() {
+        assert_eq!(1 + 2, 3)
+    }
+
+    #[test]
+    fn test_plus_operation_without_any_dependencies2() {
+        assert_eq!(2 + 3, 5)
+    }
+
+    #[test]
+    fn test_plus_operation_without_any_dependencies3() {
+        assert_eq!(3 + 4, 7)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_plus_operation_without_any_dependencies4() {
+        assert_eq!(1 + 2, 4)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_plus_operation_without_any_dependencies5() {
+        assert_eq!(2 + 3, 6)
+    }
+
+    // Tests using *rstest* crate.
+    #[rstest]
+    #[case(1, 2, 3)]
+    #[case(2, 3, 5)]
+    #[case::no_panic(3, 4, 7)]
+    #[should_panic]
+    #[case::panic(1, 2, 4)]
+    #[should_panic]
+    #[case(2,3,6)]
+    fn test_plus_operation_with_rstest(#[case] a: u32, #[case] b: u32, #[case] c: u32) {
+        assert_eq!(a + b, c)
+    }
+}
